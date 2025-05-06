2a065ae25051a63ab002e41cb13351850897e494
Luca Berhard <luca.berhard@gmx.de>
2025-05-06 15:27:36 +0200

    Added database adapter

---
+++ app/app/calc.py
@@ -0,0 +1,27 @@
+"""
+    Calculator module for performing basic arithmetic operations.
+"""
+
+
+def add(x, y):
+    """Add x and y and return the result
+    Args:
+        x (int or float): The first number.
+        y (int or float): The second number.
+    Returns:
+        int or float: The sum of x and y.
+    """
+    return x + y
+
+def subtract(x, y):
+    """Subtract x from y and return the result
+    Args:
+        x (int or float): The first number.
+        y (int or float): The second number.
+    Returns:
+        int or float: The difference of x and y.
+    """
+    return y - x
+
+
+
