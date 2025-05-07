95a2a63ee1cbd17cceec3a5494ef148be7922214
Luca Berhard <luca.berhard@gmx.de>
2025-05-07 14:55:01 +0200

    Fixed lints

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,12 +1,12 @@
 """
 Django command to wait for the database to be available.
-This command is useful for ensuring that the database is up and running before starting the application.
 """
 
 import time
 from psycopg2 import OperationalError as Psycopg20pError
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
+
 
 class Command(BaseCommand):
     """Django command to wait for the database to be available."""
